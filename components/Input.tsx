543a5e85a0899486710bd205568d8601382bad07
Divya Ganta <teja.information@gmail.com>
2026-02-13 11:36:17 +0000

    setup

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export default function Input(props: React.InputHTMLAttributes<HTMLInputElement>){
   return <input {...props} className={`w-full p-2 rounded bg-white/5 ${props.className ?? ''}`} />
 }
